1496ec7b37ca585624a4ee4360e4a64642eb7098
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2022-06-27 21:55:46 +0700

    UFO Grendizer and Control

--- Superjamegamspread/Assets/Script/Player/Controller.cs
+++ Superjamegamspread/Assets/Script/Player/Controller.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 500.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(x * speed * Time.deltaTime, 0, 0);
+
+        float z = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, 0, z * speed * Time.deltaTime);
     }
 }
